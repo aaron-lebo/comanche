a24779c987835164890d85a87a4fce48961646c9
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2017-06-24 07:45:36 -0500

    added blocks

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ let eye = [0.0, 0.0, 5.0];
 let center = [0.0, 0.0, -0.1];
 let up = [0.0, 1.0, 0.0];
 
-function move() {
+let move = _ => {
     let speed = 0.3;
     if (keys.w) vec3.add(eye, eye, vec3.scale([], center, speed));
     if (keys.a) vec3.sub(eye, eye, vec3.scale([], vec3.normalize([], vec3.cross([], center, up)), speed));
@@ -38,21 +38,38 @@ document.addEventListener('mousemove', ({movementX, movementY}) => {
     ]);
 }, false);
 
-let positions = [
-    [-0.5, +0.5, +0.5], [+0.5, +0.5, +0.5], [+0.5, -0.5, +0.5], [-0.5, -0.5, +0.5], // pos z face
-    [+0.5, +0.5, +0.5], [+0.5, +0.5, -0.5], [+0.5, -0.5, -0.5], [+0.5, -0.5, +0.5], // pos x
-    [+0.5, +0.5, -0.5], [-0.5, +0.5, -0.5], [-0.5, -0.5, -0.5], [+0.5, -0.5, -0.5], // neg z
-    [-0.5, +0.5, -0.5], [-0.5, +0.5, +0.5], [-0.5, -0.5, +0.5], [-0.5, -0.5, -0.5], // neg x
-    [-0.5, +0.5, -0.5], [+0.5, +0.5, -0.5], [+0.5, +0.5, +0.5], [-0.5, +0.5, +0.5], // top
-    [+0.5, -0.5, +0.5], [-0.5, -0.5, -0.5], [+0.5, -0.5, -0.5], [-0.5, -0.5, +0.5]  // bottom
+let block = [
+    [[-0.5, +0.5, +0.5], [+0.5, +0.5, +0.5], [+0.5, -0.5, +0.5], [-0.5, -0.5, +0.5]], // pos z face
+    [[+0.5, +0.5, +0.5], [+0.5, +0.5, -0.5], [+0.5, -0.5, -0.5], [+0.5, -0.5, +0.5]], // pos x
+    [[+0.5, +0.5, -0.5], [-0.5, +0.5, -0.5], [-0.5, -0.5, -0.5], [+0.5, -0.5, -0.5]], // neg z
+    [[-0.5, +0.5, -0.5], [-0.5, +0.5, +0.5], [-0.5, -0.5, +0.5], [-0.5, -0.5, -0.5]], // neg x
+    [[-0.5, +0.5, -0.5], [+0.5, +0.5, -0.5], [+0.5, +0.5, +0.5], [-0.5, +0.5, +0.5]], // top
+    [[+0.5, -0.5, +0.5], [-0.5, -0.5, -0.5], [+0.5, -0.5, -0.5], [-0.5, -0.5, +0.5]]  // bottom
 ];
 
+let positions = [];
 let elements = [];
-for (let i = 0; i <= 20; i += 4) {
-    elements = elements.concat([i, i+1, i+2], [i, i+2, i+3]);
+let addBlock = (x, y, z) => {
+    let idx = positions.length;
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < block[i].length; j++) {
+            let pos = block[i][j];
+            positions.push([pos[0]+x, pos[1]+y, pos[2]+z]);
+        }
+        elements.push([idx, idx+1, idx+2]);
+        elements.push([idx, idx+2, idx+3]);
+        idx += 4;
+    }
+};
+
+let size = 10;
+for (let x = -size; x <= size; x++) {
+    for (let z = -size; z <= size; z++) {
+        addBlock(x, 0, z);
+    }
 }
 
-let drawCube = regl({
+let draw = regl({
     frag: `
         precision mediump float;
         void main () {
@@ -87,5 +104,5 @@ regl.frame(_ => {
         color: [0, 0, 0, 255],
         depth: 1
     });
-    drawCube();
+    draw();
 });
