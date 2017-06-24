4340ba3421f9c7b564237567e022f9283fff0211
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2017-06-24 01:20:16 -0500

    added mouse

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import {mat4, vec3} from 'gl-matrix';
 let regl = require('regl')();
 
 let keys = {};
-document.onkeydown = evt => keys[evt.key] = true;
-document.onkeyup = evt => keys[evt.key] = false;
+document.onkeydown = ({key}) => keys[key] = true;
+document.onkeyup = ({key}) => {
+    keys[key] = false;
+    if (key == ' ') document.body.requestPointerLock();
+};
 
 let eye = [0.0, 0.0, 5.0];
-let center = [0.0, 0.0, -1.0];
+let center = [0.0, 0.0, -0.1];
 let up = [0.0, 1.0, 0.0];
 
 function move() {
@@ -15,7 +18,25 @@ function move() {
     if (keys.a) vec3.sub(eye, eye, vec3.scale([], vec3.normalize([], vec3.cross([], center, up)), speed));
     if (keys.s) vec3.sub(eye, eye, vec3.scale([], center, speed));
     if (keys.d) vec3.add(eye, eye, vec3.scale([], vec3.normalize([], vec3.cross([], center, up)), speed));
-}
+};
+
+let [pitch, yaw] = [0.0, -90.0];
+
+document.addEventListener('mousemove', ({movementX, movementY}) => {
+    let sensitivity = 0.05;
+    yaw += sensitivity * movementX;
+    pitch += sensitivity * movementY;
+    if (pitch > 89.0) pitch = 89.0;
+    else if (pitch < -89.0) pitch = -89.0;
+    let radians = x => x * Math.PI / 180.0;
+    let cos = x => Math.cos(radians(x));
+    let sin = x => Math.sin(radians(x));
+    center = vec3.normalize([], [
+        cos(yaw) * cos(pitch),
+        sin(pitch),
+        sin(yaw) * cos(pitch)
+    ]);
+}, false);
 
 let positions = [
     [-0.5, +0.5, +0.5], [+0.5, +0.5, +0.5], [+0.5, -0.5, +0.5], [-0.5, -0.5, +0.5], // pos z face
@@ -60,7 +81,7 @@ let drawCube = regl({
     }
 });
 
-regl.frame(() => {
+regl.frame(_ => {
     move();
     regl.clear({
         color: [0, 0, 0, 255],
