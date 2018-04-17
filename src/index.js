e0b5d7583f03592b041ab514f8022adb8c8218cf
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2018-04-17 11:35:51 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ let block = {
         precision mediump float;
         out vec4 color;
         void main() {
-            color = vec4(10, 1.0, 1.0, 1.0);
+            color = vec4(1.0, 1.0, 1.0, 1.0);
         }`
 };
 
